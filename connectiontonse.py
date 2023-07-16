293d9a199e81be3a19ce27db13cc5c96935a110f
pravinsharma01 <pravinsharma01@gmail.com>
2023-07-16 16:37:24 +0530

    x

--- connectiontonse.py
+++ connectiontonse.py
@@ -4,6 +4,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import nse
 
+
+nse.nse()
 df, current_market_price = nse.convertintodataframe()
 st.write(""" #My first App *""")
 st.dataframe(df)
