2545d45e19476a790d2da7686e76d3d60315fc20
pravinsharma01 <139038899+pravinsharma01@users.noreply.github.com>
2023-07-24 22:54:00 +0530

    Update connectiontonse.py

--- connectiontonse.py
+++ connectiontonse.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup
-import nse
 
-
-nse.nse()
-df, current_market_price = nse.convertintodataframe()
 st.write(""" #My first App *""")
 st.dataframe(df)
